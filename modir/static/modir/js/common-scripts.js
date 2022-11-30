1ed0291a34ff2936a2f9e961a88085c81c749f03
Emad <emadlashkari.82@gmail.com>
2022-11-30 20:02:51 +0330

    commit pyc files

--- modir/static/modir/js/common-scripts.js
+++ modir/static/modir/js/common-scripts.js
@@ -76,28 +76,28 @@ var Script = function () {
 
 // widget tools
 
-    jQuery('.widget .tools .icon-chevron-down').click(function () {
-        var el = jQuery(this).parents(".widget").children(".widget-body");
-        if (jQuery(this).hasClass("icon-chevron-down")) {
-            jQuery(this).removeClass("icon-chevron-down").addClass("icon-chevron-up");
-            el.slideUp(200);
-        } else {
-            jQuery(this).removeClass("icon-chevron-up").addClass("icon-chevron-down");
-            el.slideDown(200);
-        }
-    });
+//     jQuery('.widget .tools .icon-chevron-down').click(function () {
+//         var el = jQuery(this).parents(".widget").children(".widget-body");
+//         if (jQuery(this).hasClass("icon-chevron-down")) {
+//             jQuery(this).removeClass("icon-chevron-down").addClass("icon-chevron-up");
+//             el.slideUp(200);
+//         } else {
+//             jQuery(this).removeClass("icon-chevron-up").addClass("icon-chevron-down");
+//             el.slideDown(200);
+//         }
+//     });
 
-    jQuery('.widget .tools .icon-remove').click(function () {
-        jQuery(this).parents(".widget").parent().remove();
-    });
+//     jQuery('.widget .tools .icon-remove').click(function () {
+//         jQuery(this).parents(".widget").parent().remove();
+//     });
 
-//    tool tips
+// //    tool tips
 
-    $('.tooltips').tooltip();
+//     $('.tooltips').tooltip();
 
-//    popovers
+// //    popovers
 
-    $('.popovers').popover();
+//     $('.popovers').popover();
 
 
 
